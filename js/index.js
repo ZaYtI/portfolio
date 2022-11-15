cdca0424618fb8872865282b44b5bc6078fd1f72
antonin <99360974+ZaYtI@users.noreply.github.com>
2022-11-15 17:29:21 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
+var options = {
+    rootMargin: '0px',
+    threshold: 0.5
+}
+const callback=(entries,observer)=>{
+    entries.forEach(entry=>{
+        const {target} = entry
+
+        if(entry.intersectionRatio >= 0.5){
+            target.classList.add("is-visible")
+        }
+    })
+}
+var observer = new IntersectionObserver(callback, options);
+
+
+const elements = document.querySelectorAll(".element")
+elements.forEach((element,index)=>{
+    observer.observe(element)
+})
+
 document.querySelectorAll('.js-slide').forEach(function(thumbnailElement) {
     thumbnailElement.addEventListener('click', function(event) {
         event.preventDefault()
